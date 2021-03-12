02794590d023852cd75dfbb02cc2a5994d2d3369
FatoomMFayad <fatoomfayyad@gmail.com>
2021-03-12 09:59:55 +0200

    list rendering

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -24,13 +24,17 @@
 <h2 v-else-if="num < 0">The number is Negative</h2>
 <h2 v-else-if="num > 0">The number is Positive</h2>
 <h2 v-else>Not a number</h2>
-<!-- 
-<template v-if="display">
-  <h2>Fatoom</h2>
-  <h2>SWE</h2>
-  <h2>Full Stack</h2>
-</template> -->
 <h2 v-show="display">Using v-show</h2>
+<h2 v-for="(name, index) in names" :key="name">{{index}} {{ name }}</h2>
+<h2 v-for="name in fullNames" :key="name.first">{{ name.first}} {{name.last}}</h2>
+<div v-for="actor in actors" :key="actor.name">
+  <h2>{{actor.name}}</h2> 
+  <ul v-for="movie in actor.movies" :key="movie">
+    <li>{{ movie}}</li>
+  </ul>
+</div>
+<h2 v-for="(value, key) in myInfo" :key="value"> {{key}} {{ value }}</h2>
+
 
 </template>
 
@@ -56,7 +60,27 @@ export default {
         padding: '20px'
       },
       num: "5nnn",
-      display: true
+      display: true,
+      names:['Fatoom', 'Hanan', 'Mohammed'],
+      fullNames:[
+        {first: 'Fatoom', last: 'Fayad'},
+        {first: 'Hanan', last: 'Fayad'},
+        {first: 'Mohammed', last: 'Fayad'},
+      ],
+      actors:[
+        {
+          name: 'Christian Bale',
+          movies: ['Batman', 'The Prestige']
+        },
+        {
+          name: 'Johny Depp',
+          movies: ['Pirates of the Carrebian', 'The Tourist']
+        }
+      ],
+      myInfo:{
+        name: 'Fatoom',
+        job: 'Software Engineer'
+      }
     };
   },
 };
