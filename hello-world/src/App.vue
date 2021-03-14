710125c3b65333b6930ed801010cd6ea7b8fdc21
Fatoom Fayad <fatoomfayyad@gmail.com>
2021-03-14 11:20:28 +0200

    Event Handling

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <h2>{{ 2 + 3 + 5}}</h2>
-  <h2>{{ 10 + 15 + 5}}</h2>
-  <h2>Add method - {{ add(2, 3, 7) }}</h2>
-  <h2>Multiply method - {{ multiply(this.baseValue) }}</h2>
+  <h2>{{ name }}</h2>
+  <div>
+    <button v-on:click="name= 'Hanan' ">Change Name</button>
+  </div>
+  <h2>{{ count }}</h2>
+  <div>
+     <button v-on:click="increment( 1 )">Increment 1 </button>
+     <button v-on:click="increment( 5 )">Increment 5</button>
+     <button v-on:click="decrement( 1 )">Decrement 1</button>
+     <button v-on:click="decrement( 5 )">Decrement 5</button>
+  </div>
+  
 </template>
 
 <script>
@@ -11,17 +19,17 @@ export default {
   name: 'App',
   data(){
     return {
-      baseMultiplier: 5,
-      baseValue: 2
+      name: "Fatoom",
+      count: 0
     };
   },
   methods: {
-    add(a, b, c) {
-      return a + b + c;
+    increment( num ) {
+      return this.count+= num
     },
-    multiply(num) {
-      return num * this.baseMultiplier;
-    }
+    decrement( num ) {
+      return this.count-= num
+    },
   },
 };
 </script>
