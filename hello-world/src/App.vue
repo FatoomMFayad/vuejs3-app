acbe968835d24ed0bada085d3ee18342a7de07d2
ffayad <ffayad@ptcdb.edu.ps>
2021-03-14 10:23:56 +0200

    methods

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,46 +1,8 @@
 <template>
-<div>
-  {{greet}} {{name}}
-</div>
-<div v-html="position"></div>
-<h2 v-bind:id="headingId">Heading</h2>
-<button v-bind:disabled="isDisabled">Bind</button> 
-<h2 class="underline">Underlined Text</h2>
-<h2 v-bind:class="status">Status</h2>
-<h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
-<h2 v-bind:class="isSoldOut? 'sold-out' : 'new'">Sold out Movie</h2>
-<h2 v-bind:class="[isPromoted && 'promoted', isSoldOut? 'sold-out' : 'new']">Array conditional Movie</h2>
-<h2 v-bind:class="{
-  promoted: isPromoted,
-  new: !isSoldOut,
-  'sold-out': isSoldOut
-}">Object Conditional Movie</h2>
-<h2 v-bind:style="{
-  color: highlightColor,
-  'font-size': headerSize + 'px'
-}">Inline Style</h2>
-<h2 v-bind:style="headerStyle">Style Object</h2>
-<h2 v-if="num === 0">The number is zero</h2>
-<h2 v-else-if="num < 0">The number is Negative</h2>
-<h2 v-else-if="num > 0">The number is Positive</h2>
-<h2 v-else>Not a number</h2>
-<h2 v-show="display">Using v-show</h2>
-
-  <template v-for="(name, index) in names" :key="name">
-    <h2 v-if="name === 'Fatoom'">
-      {{index}} {{ name }}
-    </h2>    
-  </template>
-<h2 v-for="name in fullNames" :key="name.first">{{ name.first}} {{name.last}}</h2>
-<div v-for="actor in actors" :key="actor.name">
-  <h2>{{actor.name}}</h2> 
-  <ul v-for="movie in actor.movies" :key="movie">
-    <li>{{ movie}}</li>
-  </ul>
-</div>
-<h2 v-for="(value, key) in myInfo" :key="value"> {{key}} {{ value }}</h2>
-
-
+  <h2>{{ 2 + 3 + 5}}</h2>
+  <h2>{{ 10 + 15 + 5}}</h2>
+  <h2>Add method - {{ add(2, 3, 7) }}</h2>
+  <h2>Multiply method - {{ multiply(this.baseValue) }}</h2>
 </template>
 
 <script>
@@ -49,44 +11,17 @@ export default {
   name: 'App',
   data(){
     return {
-      greet: "Hello",
-      name: "Fatoom",
-      position: "<b>Software Engineer</b>",
-      headingId: "heading",
-      isDisabled: false,
-      status: "success",
-      isPromoted: true,
-      isSoldOut: true,
-      highlightColor: "orange",
-      headerSize: 50,
-      headerStyle:{
-        color: "orange",
-        fontSize: '50px',
-        padding: '20px'
-      },
-      num: "5nnn",
-      display: true,
-      names:['Fatoom', 'Hanan', 'Mohammed'],
-      fullNames:[
-        {first: 'Fatoom', last: 'Fayad'},
-        {first: 'Hanan', last: 'Fayad'},
-        {first: 'Mohammed', last: 'Fayad'},
-      ],
-      actors:[
-        {
-          name: 'Christian Bale',
-          movies: ['Batman', 'The Prestige']
-        },
-        {
-          name: 'Johny Depp',
-          movies: ['Pirates of the Carrebian', 'The Tourist']
-        }
-      ],
-      myInfo:{
-        name: 'Fatoom',
-        job: 'Software Engineer'
-      }
+      baseMultiplier: 5,
+      baseValue: 2
     };
+  },
+  methods: {
+    add(a, b, c) {
+      return a + b + c;
+    },
+    multiply(num) {
+      return num * this.baseMultiplier;
+    }
   },
 };
 </script>
